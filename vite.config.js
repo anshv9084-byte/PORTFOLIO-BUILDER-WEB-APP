3bd8153feaf4d58b91fbd4bbe81104917a6da3e2
anshv9084-byte <anshv9084@gmail.com>
2026-03-13 20:52:55 +0530

    CHANGES

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,18 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: './',
   root: '.',
   server: {
     port: 3000,
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve('index.html'),
+        landing: resolve('landing.html'),
+      },
+    },
   }
 })
